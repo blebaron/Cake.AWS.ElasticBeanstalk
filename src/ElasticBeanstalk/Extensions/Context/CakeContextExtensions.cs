0f2d9485438547049ceeca33ae0153f4dc73a45d
Brian LeBaron <brian.a.lebaron@gmail.com>
2017-10-26 17:11:45 -0600

    Add create settings extension method and update nuspec

---
+++ src/ElasticBeanstalk/Extensions/Context/CakeContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Cake.Core;
+
+namespace Cake.AWS.ElasticBeanstalk
+{
+    public static class CakeContextExtensions
+    {
+        public static ElasticBeanstalkSettings CreateElasticBeanstalkSettings(this ICakeContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return context.Environment.CreateElasticBeanstalkSettings();
+        }
+    }
+}
